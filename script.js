8620e15ae06b5fc8a8137b8beb0c6076cf7592c4
mdmn07C5 <malicdan@protonmail.ch>
2024-03-21 17:40:18 -0400

    Adds win condition checking for row and column

--- script.js
+++ script.js
@@ -15,7 +15,15 @@ const Gameboard = function () {
         if (board[x][y] === ' ') board[x][y] = mark;
     }
 
-    return { printToConsole, markPosition }
+    const isWon = (x, y) => {
+        row_win = board[x][0] === board[x][1] && board[x][1] === board[x][2];
+
+        col_win = board[0][y] === board[1][y] && board[1][y] === board[2][y];
+
+        return row_win || col_win;
+    }
+
+    return { printToConsole, markPosition, isWon }
 }();
 
 const Gameloop = function () {
@@ -26,6 +34,9 @@ function createPlayer () {
 
 };
 
-Gameboard.markPosition('x', 1, 1);
+Gameboard.markPosition('x', 0, 0);
+Gameboard.markPosition('x', 1, 0);
+Gameboard.markPosition('x', 2, 0);
 Gameboard.markPosition('o', 1, 1);
 Gameboard.printToConsole();
+console.log(Gameboard.isWon(0, 2));
