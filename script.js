88a8fcd4c9742bfd0b524545919ef1fcb92fb1c9
mdmn07C5 <malicdan@protonmail.ch>
2024-03-21 17:13:38 -0400

    Adds 2d array to Gameboard, way to log board
    Adds 2d array to Gameboard with temp value
    Adds method to log to board for debug purposes

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const Gameboard = function () {
+    let board = [
+        ['t','t','t'],
+        ['m','m','m'],
+        ['b','b','b']
+    ];
 
+    const printToConsole = () => {
+        board.forEach((row) => {
+            console.log(row.join(' | '));
+        });
+    }
+
+    return { printToConsole }
 }();
 
 const Gameloop = function () {
@@ -9,3 +21,5 @@ const Gameloop = function () {
 function createPlayer () {
 
 };
+
+Gameboard.printToConsole();
